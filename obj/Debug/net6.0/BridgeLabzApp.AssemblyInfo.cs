2943aa6a419c209e23e2f4ecc1fcaa61ab704b8d
KOTIPALLI SRIKESH <125877167+srikesh2k4@users.noreply.github.com>
2025-11-28 14:27:49 +0530

    Dev

--- obj/Debug/net6.0/BridgeLabzApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/BridgeLabzApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830bdb6a3808d7265fc4cf3846ae3deafb65510d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
