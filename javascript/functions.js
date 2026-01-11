bc3b48f7b9d3b7d94e5e20fcf6310f11497c99d2
KOTIPALLI SRIKESH <125877167+srikesh2k4@users.noreply.github.com>
2026-01-11 21:41:40 +0530

    parameters

---
+++ javascript/functions.js
@@ -0,0 +1,5 @@
+export function sum(a, b) {
+return a + b
+}
+
+export const multiply = (a, b) => a * b
---
+++ javascript/parameters.js
@@ -0,0 +1,2 @@
+export const greet = (name = "User") => name
+export const total = (...nums) => nums.reduce((a, b) => a + b, 0)
