1841f524fc0e888a68965972456b0576ccbb69e7
KOTIPALLI SRIKESH <125877167+srikesh2k4@users.noreply.github.com>
2026-01-11 21:37:02 +0530

    Datatype

---
+++ javascript/data-types.js
@@ -0,0 +1,8 @@
+export const str = "Angular"
+export const num = 100
+export const bool = true
+export const nul = null
+export const undef = undefined
+export const sym = Symbol("id")
+export const obj = { key: "value" }
+export const arr = [1, 2, 3]
